78ad9979910bcc9553f01ecc4f74234a559be510
Tanbhir Tahmid <tanbhir.tahmid@gmail.com>
2026-05-24 20:25:55 +0600

    Test fast loop in c++

---
+++ CPP/fast_loop.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vint = vector<int>;
+using vllint = vector<long long int>;
+using vchar = vector<char>;
+using vstring = vector<string>;
+#define pb push_back
+#define endn '\n'
+#define all(x) (x).begin(), (x).end()
+
+// Fast I/O setup
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main() {
+    fastIO();
+
+    int arr[]={1, 2, 54, 343};
+
+    for(int i : arr)
+    {
+        cout << i << endn;
+    }
+
+    return 0;
+}
